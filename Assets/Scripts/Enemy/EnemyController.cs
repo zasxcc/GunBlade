87ecda28380ffbfc52a16cfd627ec5acec59f699
PIH <zasxcc@naver.com>
2020-11-02 19:28:07 +0900

    Enemy AI script
    Error : 플레이하면 멈춤

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public enum CurrentState {  idle, move, attack, dead };
+    private CurrentState currState = CurrentState.idle;
+
+    public int HP;
+    public float speed;
+    private bool isDead = false;
+    public float traceDistance = 15.0f;
+    public float attackDistance = 3.2f;
+
+
+    private NavMeshAgent nvAgent;
+    private Transform tr;
+    private Transform targetTransform;
+    private Animator anim;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+
+        //추적할 오브젝트 태그
+        targetTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        nvAgent = GetComponent<NavMeshAgent>();
+
+        //추적 position 설정
+        nvAgent.destination = targetTransform.position;
+
+        StartCoroutine(CheckState());
+        StartCoroutine(CheckStateForAction());
+    }
+
+    IEnumerator CheckState()
+    {
+        while(!isDead)
+        {
+            yield return new WaitForSeconds(0.3f);
+
+            //타겟과의 거리
+            float dist = Vector3.Distance(targetTransform.position, tr.position);
+
+            if(dist <= attackDistance)
+            {
+                currState = CurrentState.attack;
+
+                //애니메이션적용
+
+                //
+            }
+            else if(dist <= traceDistance)
+            {
+                currState = CurrentState.move;
+
+                //애니메이션적용
+
+                //
+            }
+            else
+            {
+                currState = CurrentState.idle;
+
+                //애니메이션적용
+
+                //
+            }
+        }
+    }
+
+    IEnumerator CheckStateForAction()
+    {
+        while(!isDead)
+        {
+            switch(currState)
+            {
+                case CurrentState.idle:
+                    nvAgent.Stop();
+                    break;
+                case CurrentState.move:
+                    nvAgent.SetDestination(targetTransform.position);
+                    nvAgent.Resume();
+                    break;
+                case CurrentState.attack:
+                    break;
+            }
+        }
+        yield return null;
+    }
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+       
+    }
+}
