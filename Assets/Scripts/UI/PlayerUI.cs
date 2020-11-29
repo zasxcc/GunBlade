f49d6661c9373478b3f3497c6b49e4f5e0de2c33
PIH <zasxcc@naver.com>
2020-11-29 17:10:14 +0900

    Target 혹은 Player의 HPBar UI

---
+++ Assets/Scripts/UI/PlayerUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    Slider sliderHP;
+    float fSliderBarTime;
+    public PlayerController player;
+
+    void Start()
+    {
+        sliderHP = GetComponent<Slider>();
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        sliderHP.value = player.HP;
+    }
+}
