5f3b44ae64f2b0f056ef136bedd8290680d58c52
@spadille434 <spadille434@gmail.com>
2020-12-14 21:23:51 +0900

    Title Scene

---
+++ Assets/Scripts/UI/TitleSceneManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    public string sceneName_Start = "";
+    public string sceneName_HighScore = "";
+
+    public void OnClickButton_Start()
+    {
+        Debug.Log("TitleSceneManager.OnClickButton_Start() is Called");
+        SceneManager.LoadScene(sceneName_Start);
+    }
+
+    public void OnClickButton_HighScore()
+    {
+        Debug.Log("TitleSceneManager.OnClickButton_HighScore() is Called");
+        SceneManager.LoadScene(sceneName_HighScore);
+    }
+
+    public void OnClickButton_Exit()
+    {
+        Debug.Log("TitleSceneManager.OnClickButton_Exit() is Called");
+        Application.Quit();
+    }
+}
